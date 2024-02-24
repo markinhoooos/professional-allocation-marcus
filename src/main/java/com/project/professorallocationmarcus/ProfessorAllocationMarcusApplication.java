bba11a66489c0b3a25bc4127effbbdfd0765d163
Marcus Diogenes G. da Silva <marcus.2320@outlook.com>
2024-02-24 14:29:34 -0300

    add project

---
+++ src/main/java/com/project/professorallocationmarcus/ProfessorAllocationMarcusApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocationmarcus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationMarcusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationMarcusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professorallocationmarcus/ProfessorAllocationMarcusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professorallocationmarcus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationMarcusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
